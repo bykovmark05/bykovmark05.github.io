a0668a3fa85a349d33a968d9e177cf9087217f00
bykovmark05 <bykovmark05@gmail.com>
2025-05-27 04:08:20 +0300

    Add files via upload

---
+++ n1.js
@@ -0,0 +1,12 @@
+function max(a, b) {
+    if (a > b) {
+        return a;
+    } else {
+        return b;
+    }
+}
+
+console.log(max(1, 15));
+console.log(max(1000, 500));
+console.log(max(-10, -50));
+console.log(max(0, 0));
